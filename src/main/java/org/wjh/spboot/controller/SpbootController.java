507a6153e2bcf9c73dc60071334e90a9124f3da1
王吉辉 <wangjihui>
2022-08-26 17:26:17 +0800

    optimize

--- src/main/java/org/wjh/spboot/controller/SpbootController.java
+++ src/main/java/org/wjh/spboot/controller/SpbootController.java
@@ -2,7 +2,6 @@ package org.wjh.spboot.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +11,8 @@ public class SpbootController {
     Logger logger = LoggerFactory.getLogger(SpbootController.class);
 
     @RequestMapping("/spboot/get.do")
-    public String get(@RequestHeader("user-agent") String userAgent, @RequestHeader("cookie") String cookie){
+    public String get(@RequestHeader("user-agent") String userAgent,
+                      @RequestHeader("cookie") String cookie){
         logger.info("cookie={};  userAgent={}", cookie, userAgent);
         return userAgent +"\n  cookie: " + cookie;
     }
